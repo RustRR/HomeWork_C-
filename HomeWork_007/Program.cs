b91c800d9d70c1cdda62ce8c3046020377d8445c
RustR <reimovrr@mail.ru>
2023-03-15 00:58:03 +0600

    Задача 15

--- HomeWork_007/Program.cs
+++ HomeWork_007/Program.cs
@@ -13,8 +13,8 @@ int number = Prompt("Введите число недели");
 string ss = "Это не день недели";
 string check(int number)
 {
-if (number >= 6 && number < 8) ss = "Это выходной";
-if (number >= 1 && number < 7) ss = "Это будний день";
+if (number >= 5 && number < 8) ss = "Это выходной";
+if (number >= 1 && number < 6) ss = "Это будний день";
 return ss;
 }
 System.Console.WriteLine(check(number));
