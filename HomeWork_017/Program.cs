7cb1fd2ecaea6ffb08a9040be71ea153718babb7
RustR <reimovrr@mail.ru>
2023-03-26 21:45:02 +0600

    Задача 41

---
+++ HomeWork_017/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[a];
+
+void massif(int a)
+{
+    for (int i = 0; i < a; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива ");
+        Array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+int sum(int[] Array)
+{
+    int i = 0;
+    int sum = 0;
+    while (i < Array.Length)
+    {
+        if (Array[i] > 0)
+            sum = sum + 1;
+        i = i + 1;
+    }
+    return sum;
+}
+massif(a);
+Console.Write($"\nЧисел больше нуля: {sum(Array)}");
