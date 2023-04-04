9de308b6259a1c06907b40c3c73e7f06afe1e82c
RustR <reimovrr@mail.ru>
2023-04-04 23:43:39 +0600

    Задача 62

--- HomeWork_026/Program.cs
+++ HomeWork_026/Program.cs
@@ -4,3 +4,40 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+int n = 4;
+int[,] sqareMatrix = new int[n, n];
+
+int temp = 1;
+int a = 0;
+int b = 0;
+
+while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+{
+    sqareMatrix[a, b] = temp;
+    temp++;
+    if (a <= b + 1 && a + b < sqareMatrix.GetLength(1) - 1)
+        b++;
+    else if (a < b && a + b >= sqareMatrix.GetLength(0) - 1)
+        a++;
+    else if (a >= b && a + b > sqareMatrix.GetLength(1) - 1)
+        b--;
+    else
+        a--;
+}
+WriteArray(sqareMatrix);
+
+void WriteArray(int[,] array)
+{
+    for (int a = 0; a < array.GetLength(0); a++)
+    {
+        for (int b = 0; b < array.GetLength(1); b++)
+        {
+            if (array[a, b] / 10 <= 0)
+                Console.Write($" {array[a, b]} ");
+
+            else Console.Write($"{array[a, b]} ");
+        }
+        Console.WriteLine();
+    }
+}
